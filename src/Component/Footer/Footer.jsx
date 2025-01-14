b097a4f553e2f29ce44711ec0e9c8570985438bc
karthikn25 <mkarthikeyan179@gmail.com>
2025-01-14 12:57:48 +0530

    footer part completed

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div className='footer'>
-    <p className='footer-content'>KarthiknProjects@2025</p>
+    <div className="footer">
+      <a href="https://github.com/karthikn25" target="_blank">
+        <i className="bx bxl-github"></i>
+      </a>
+      <p className="footer-content">KarthiknProjects@2025</p>
     </div>
-  )
+  );
 }
